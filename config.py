6a21bb8741699e04de822fdb4473b4c0f0e7b430
agjayant <agjayant@rediff.com>
2017-02-23 20:12:12 +0530

    added activation and pool params

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ channels = 1
 layers = [ ('conv',6, 5, 1), ('pool',2,2), ('conv',16,5, 1 ),('pool',2,2),('fc',120),('fc',84), ('softmax',10)  ]
 # layers = [ ('conv',6, 5, 1), ('subs',2,2), ('conv',16,5, 1 ),('subs',2,2),('conv_fc',16,5,120),('fc_fc',120,84), ('softmax',10)  ]
 
+activation = 'relu'
+pool = 'max' # 'mean' or 'max'
 #Network Initialisation
 minW = -0.1
 maxW =  0.1       # Network weights are initialised in range [minW,maxW]
