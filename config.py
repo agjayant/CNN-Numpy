db1cb5a1c4c2ee067f620181d070939f72283b7f
agjayant <agjayant@rediff.com>
2017-02-23 21:12:19 +0530

    removed extra comments

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ channels = 1
 
 #Layers
 layers = [ ('conv',6, 5, 1), ('pool',2,2), ('conv',16,5, 1 ),('pool',2,2),('fc',120),('fc',84), ('softmax',10)  ]
-# layers = [ ('conv',6, 5, 1), ('subs',2,2), ('conv',16,5, 1 ),('subs',2,2),('conv_fc',16,5,120),('fc_fc',120,84), ('softmax',10)  ]
 
 activation = 'relu'
 pool = 'max' # 'mean' or 'max'
+
 #Network Initialisation
 minW = -0.1
 maxW =  0.1       # Network weights are initialised in range [minW,maxW]
