better initialisation, correct backward pass softmax, grad check script

src/back.py
@@ -14,7 +14,7 @@ def conv(X, convFilters):
         depth = len(convFilter)
         assert(depth == len(X)), 'Dimension Mismatch'
         for j in range(depth):
-            featureMap.append(signal.convolve2d(X[j], convFilter[j],'valid'))
+            featureMap.append(signal.convolve2d(X[j], np.rot90(convFilter[j],2),'valid'))
 
         featureMaps.append( sum(featureMap) )
 
@@ -37,7 +37,7 @@ def convBack(X, dy, W):
     for i in range(W.shape[0]):
         kernel = []
         for j in range(W.shape[1]):
-            kernel.append( signal.convolve2d(X[j], dy[i],'valid') )
+            kernel.append( signal.convolve2d(X[j], np.rot90(dy[i],2) ,'valid') )
             Wb[j,i] = np.rot90(W[i,j],2)
         dW.append(np.asarray(kernel))
         dB.append(np.sum(dy[i]))

src/convnet.py
@@ -23,28 +23,33 @@ lr = config.lr
 class cnn:
     def __init__(self):
 
-        self.Weights = [np.random.uniform(minW,maxW,size=(layers[0][1],inp_channels,layers[0][2],layers[0][2]))]
+        n = inp_width*inp_height
+        self.Weights = [np.random.randn(layers[0][1],inp_channels,layers[0][2],layers[0][2])/np.sqrt(n)]
         out_Size =  inp_width - layers[0][2] + 1 ########### Only for Height = Width
         self.Biases = [initBias*np.ones( layers[0][1] )]
 
         self.poolParams = [(layers[1][1], layers[1][2])]
         out_Size = out_Size/2  ########## Only for Kernel = 2 and Stride = 2
 
-        self.Weights.append(np.random.uniform(minW,maxW,size=(layers[2][1],layers[0][1],layers[2][2],layers[2][2])))
+        n = out_Size*out_Size*layers[0][1]
+        self.Weights.append(np.random.randn(layers[2][1],layers[0][1],layers[2][2],layers[2][2])/np.sqrt(n))
         out_Size = out_Size - layers[2][2]+1
         self.Biases.append(initBias*np.ones(layers[2][1]))
 
         self.poolParams.append((layers[3][1],layers[3][2]))
         out_Size = out_Size/2  ########## Only for Kernel = 2 and Stride = 2
 
-        self.Weights.append(np.random.uniform(minW,maxW,size=(layers[4][1],layers[2][1],out_Size,out_Size)))
+        n = out_Size*out_Size*layers[2][1]
+        self.Weights.append(np.random.randn(layers[4][1],layers[2][1],out_Size,out_Size)/np.sqrt(n))
         out_Size = 1
         self.Biases.append(initBias*np.ones(layers[4][1]))
 
-        self.Weights.append(np.random.uniform(minW,maxW,size=(layers[5][1],layers[4][1])))
+        n = layers[4][1]
+        self.Weights.append(np.random.randn(layers[5][1],layers[4][1])/np.sqrt(n))
         self.Biases.append(initBias*np.ones(layers[5][1]))
 
-        self.Weights.append(np.random.uniform(minW,maxW,size=(layers[6][1],layers[5][1])))
+        n = layers[5][1]
+        self.Weights.append(np.random.randn(layers[6][1],layers[5][1])/np.sqrt(n))
         self.Biases.append(initBias*np.ones(layers[6][1]))
 
         self.Weights = np.asarray(self.Weights)
@@ -82,6 +87,10 @@ class cnn:
         layer7 -= np.max(layer7)
         layer7 = np.exp(layer7)/sum(np.exp(layer7))
         return layer7
+
+    def trloss(self, trainData, trainLabel):
+
+        return -1*sum( trainLabel * np.log(self.forward(trainData)) )
 
     def predict(self, inputVal):
 
@@ -153,7 +162,8 @@ class cnn:
             loss += -1*sum( X_label * np.log(layer7) )
 
             ### Gradients Accumulate
-            dy = X_label - layer7
+            dy = -1*(X_label - layer7)/2
+            # dy = (-1*X_label + layer7)/2
 
             [dy, dW, dB ] = fcback(layer6, np.asarray([dy]).transpose() , weights[4])
             dW4 += dW
@@ -183,8 +193,6 @@ class cnn:
             dW0 += dW
             dB0 += dB.flatten()
 
-        # print np.sum(dW0)
-        # print np.sum(dW1)
         # Updates
         weights[0] -= lr*dW0/batchSize
         weights[1] -= lr*dW1/batchSize
@@ -202,6 +210,7 @@ class cnn:
         self.Weights = weights
         self.Biases = biases
 
+        # return [loss/batchSize, dW4/batchSize]
         return loss/batchSize
 
 

src/fwd.py
@@ -17,7 +17,7 @@ def convFwd(X, convFilters, bias):
         depth = len(convFilter)
         assert(depth == len(X)), 'Dimension Mismatch'
         for j in range(depth):
-            featureMap.append(signal.convolve2d(X[j], convFilter[j],'valid'))
+            featureMap.append(signal.convolve2d(X[j], np.rot90(convFilter[j],2) ,'valid'))
         featureMap = sum(featureMap) + bias[i]*np.ones((featureMap[0].shape[0], featureMap[0].shape[1]))
 
         featureMaps.append(act.activation(featureMap, activation))
@@ -49,7 +49,7 @@ def convpool(X, convFilters, bias, kernel, stride):
         depth = len(convFilter)
         assert(depth == len(X)), 'Dimension Mismatch'
         for j in range(depth):
-            featureMap.append(signal.convolve2d(X[j], convFilter[j],'valid'))
+            featureMap.append(signal.convolve2d(X[j], np.rot90(convFilter[j],2) ,'valid'))
         featureMap = act.activation(sum(featureMap) + bias[i]*np.ones((featureMap[0].shape[0], featureMap[0].shape[1]))
  ,activation)
 